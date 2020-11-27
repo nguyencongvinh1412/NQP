ac2565bad40a843ebaca90cbe108a1b40cc99005
qnhat358 <quangnhat358@gmail.com>
2020-11-27 15:07:33 +0700

    login vs register

--- app/src/main/java/com/example/myproject/Login.java
+++ app/src/main/java/com/example/myproject/Login.java
@@ -1,64 +1,103 @@
 package com.example.myproject;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Login#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+
 public class Login extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Login() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Login.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Login newInstance(String param1, String param2) {
-        Login fragment = new Login();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private View view;
+    private EditText txt_name, txt_pass;
+    // ImageView imgGmail;
+    private Button btn_login, btn_register;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference mDatabase;
+    private Context context;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        view = (View)inflater.inflate(R.layout.fragment_login, container, false);
+        context = view.getContext();
+        setHasOptionsMenu(true);
+        FirebaseApp.initializeApp(context);
+
+        // ánh xạ các compoment
+        txt_name = view.findViewById(R.id.txt_name);
+        txt_pass = view.findViewById(R.id.txt_pass);
+        btn_login = view.findViewById(R.id.btn_login);
+        btn_register = view.findViewById(R.id.btn_register);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        // đăng ký tài khoản mới
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // lệnh nhảy sang fragment khác
+                Navigation.findNavController(view).navigate(R.id.action_login_to_register);
+            }
+        });
+
+        // login với tài khoản
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String name = txt_name.getText().toString();
+                final String pass = txt_pass.getText().toString();
+                if(name.trim().equals("") || pass.trim().equals(""))
+                {
+                    Toast.makeText(context, "Please enter information for userName and Password", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    firebaseAuth.signInWithEmailAndPassword(name, pass)
+                            .addOnCompleteListener((Activity) context, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful())
+                                    {
+                                        // login sucess
+                                        Bundle bundle = new Bundle();
+                                        bundle.putString("name", name);
+                                        bundle.putString("pass", pass);
+                                        Navigation.findNavController(view).navigate(R.id.action_login_to_home2, bundle);
+                                    }
+                                    else
+                                    {
+                                        // failed
+                                        Toast.makeText(context, "Login Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+        return  view;
     }
 }
--- app/src/main/java/com/example/myproject/Register.java
+++ app/src/main/java/com/example/myproject/Register.java
@@ -1,64 +1,66 @@
 package com.example.myproject;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Register#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class Register extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Register() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Register.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Register newInstance(String param1, String param2) {
-        Register fragment = new Register();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private View view;
+    private EditText txt_name, txt_pass;
+    private Button btn_register;
+    private Context context;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        view = (View) inflater.inflate(R.layout.fragment_register, container, false);
+        setHasOptionsMenu(true);
+        context = view.getContext();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        // anh xa
+        txt_name = view.findViewById(R.id.txt_name);
+        txt_pass = view.findViewById(R.id.txt_pass);
+        btn_register = view.findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(v -> {
+            final String name = txt_name.getText().toString();
+            final String pass = txt_pass.getText().toString();
+            firebaseAuth.createUserWithEmailAndPassword(name, pass).addOnCompleteListener((Activity) context, task -> {
+                if (task.isSuccessful()){
+                    Bundle bundle = new Bundle();
+                    bundle.putString("name", name);
+                    bundle.putString("pass", pass);
+                    Navigation.findNavController(view).navigate(R.id.action_register_to_home2, bundle);
+                }
+                else{
+                    Toast.makeText(context, "Failed", Toast.LENGTH_LONG).show();
+                }
+            });
+        });
+        return view;
     }
 }
